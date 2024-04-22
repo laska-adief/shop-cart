e90e6042baa0fb1c144a4dab5c2ddc5cb2921787
laska-adief <laskaadief.la@gmail.com>
2024-04-22 10:42:32 +0700

    [Shop Cart] create navbar component

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,10 @@
+const navbar = () => {
+  return (
+    <div className="shadow-md h-14 py-4 px-8">
+      <div className="w-full text-center">
+        <h1 className="font-bold">Shop Cart</h1>
+      </div>
+    </div>
+  );
+};
+export default navbar;
