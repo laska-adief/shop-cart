67228dee77f12a89dbc0b7e96f5a4a43af4eceed
laska-adief <laskaadief.la@gmail.com>
2024-04-22 10:44:48 +0700

    [Shop Cart] add badge quantity on icon cart in navbar component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,8 +6,11 @@ const navbar = () => {
       <div className="flex-2 w-full text-center">
         <h1 className="font-bold">Shop Cart</h1>
       </div>
-      <div className="flex-1 w-full">
+      <div className="flex-1 w-full relative">
         <ShoppingCart />
+        <div className="absolute -top-[5px] -right-[8px] bg-red-500 rounded-full w-4 h-4 flex justify-center items-center">
+          <p className="text-xs text-white">1</p>
+        </div>
       </div>
     </div>
   );
