593a1e7e782c4a68c81d9d3f52cac71ae085d5ed
laska-adief <laskaadief.la@gmail.com>
2024-04-23 20:12:46 +0700

    [Shop Cart] create routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import Products from "./pages/Products.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/products",
+        element: <Products />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode></React.StrictMode>);
