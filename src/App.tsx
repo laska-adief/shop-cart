a7705ba9f090d83e1f11a027d3a8824c790de493
laska-adief <laskaadief.la@gmail.com>
2024-04-26 07:53:03 +0700

    [Shop Cart] add Navbar component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
