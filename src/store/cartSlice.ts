e32414916dfa00c81d129216e2d8c0d203bb608d
laska-adief <laskaadief.la@gmail.com>
2024-05-04 20:43:10 +0700

    [Shop Cart] create reducer addCart in cartSlice

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -12,5 +12,21 @@ const initialState: CartSlice = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addCart: (state, action) => {
+      const cartProduct = state.product;
+      const findProduct = cartProduct.find((product: Product) => product.id === action.payload.id);
+      let tempProduct: Product;
+      if (findProduct) {
+        cartProduct.filter((product: Product) => (product.id === findProduct.id ? { ...product, quantity: product.quantity++ } : product));
+      } else {
+        tempProduct = { ...action.payload, quantity: 1 };
+        cartProduct.push(tempProduct);
+      }
+    },
+  },
 });
+
+export const { addCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
