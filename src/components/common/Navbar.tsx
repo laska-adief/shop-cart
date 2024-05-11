d50fd516af1a6cb8d00d518fcafe6d70ea30b22c
laska-adief <laskaadief.la@gmail.com>
2024-05-12 06:18:16 +0700

    [Shop Cart] get cart product length

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,6 +1,9 @@
+import { RootState } from "@/store/store";
 import { ShoppingCart } from "lucide-react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const cart = useSelector((state: RootState) => state.cart.product.length);
   return (
     <div className="shadow-md h-14 py-4 px-8 flex items-center bg-white sticky top-0 left-0 z-10">
       <div className="flex-2 w-full text-center">
@@ -8,7 +11,7 @@ const Navbar = () => {
       </div>
       <div className="flex-1 w-full relative">
         <ShoppingCart />
-        <div className="absolute -top-[5px] -right-[10px] bg-red-500 rounded-full w-5 h-5 flex justify-center items-center">
+        <div className="absolute -top-[8px] -right-[10px] bg-red-500 rounded-full w-5 h-5 flex justify-center items-center">
           <p className="text-xs text-white">1</p>
         </div>
       </div>
