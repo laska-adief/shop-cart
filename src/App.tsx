bfce9b02889abd0f642bce6a9ef9c8dc925fc9c7
laska-adief <laskaadief.la@gmail.com>
2024-05-14 09:25:52 +0700

    [Shop Cart] add ToastContainer in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/common/Navbar";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </>
   );
 }
