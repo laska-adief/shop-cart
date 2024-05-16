72477e09dd9dfe888ebe392abc6b6ab0cf5829ae
laska-adief <laskaadief.la@gmail.com>
2024-05-17 06:30:56 +0700

    [Shop Cart] add popover in navbar component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,6 +1,7 @@
 import { RootState } from "@/store/store";
 import { ShoppingCart } from "lucide-react";
 import { useSelector } from "react-redux";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const Navbar = () => {
   const cart = useSelector((state: RootState) => state.cart.product.length);
@@ -9,14 +10,23 @@ const Navbar = () => {
       <div className="flex-2 w-full text-center">
         <h1 className="font-bold">Shop Cart</h1>
       </div>
-      <div className="flex-1 w-full relative">
-        <ShoppingCart />
-        {cart > 0 && (
-          <div className="absolute -top-[8px] -right-[10px] bg-red-500 rounded-full w-5 h-5 flex justify-center items-center">
-            <p className="text-xs text-white">{cart}</p>
+      <Popover>
+        <PopoverTrigger>
+          <div className="flex-1 w-full relative">
+            <ShoppingCart />
+            {cart > 0 && (
+              <div className="absolute -top-[8px] -right-[10px] bg-red-500 rounded-full w-5 h-5 flex justify-center items-center">
+                <p className="text-xs text-white">{cart}</p>
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </PopoverTrigger>
+        <PopoverContent className="w-80 bg-white mt-6 mr-3 p-4 rounded-sm shadow-2xl">
+          Place content for the popover here. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa amet deleniti
+          doloremque, suscipit modi dignissimos officiis nobis, esse doloribus, eaque exercitationem minus mollitia optio? In libero totam
+          cumque non!
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
