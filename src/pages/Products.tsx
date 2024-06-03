3142278e1d177de4847d9bf2e531d375af7a4cdb
laska-adief <laskaadief.la@gmail.com>
2024-06-03 09:45:14 +0700

    [Shop Cart] add loading in Product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,22 +5,31 @@ import { useEffect, useState } from "react";
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const getAllProducts = async () => {
       const { data } = await axios.get("https://dummyjson.com/products");
-      setProducts(data.products);
+      if (data?.products) {
+        setIsLoading(false);
+        setProducts(data.products);
+      } else {
+        setIsLoading(false);
+      }
     };
 
     getAllProducts();
   }, []);
 
-  return (
-    <div className="p-4 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 flex-wrap gap-4 justify-start m-auto">
+  return !isLoading ? (
+    <div className="p-4 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-wrap gap-4 justify-start m-auto">
       {products.map((product: Product) => (
         <ProductCard data={product} key={product.id} />
       ))}
     </div>
+  ) : (
+    <div className="h-20 w-full flex items-center justify-center">Loading...</div>
   );
 };
 
