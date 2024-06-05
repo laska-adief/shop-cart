e7519c89c5dd15c256a9a667309bddc0572e4ec4
laska-adief <laskaadief.la@gmail.com>
2024-06-05 10:05:21 +0700

    [Shop Cart] add set quantity in card product

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,10 +1,11 @@
 import { Product } from "@/types";
 import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
 import { Button } from "../ui/button";
-import { ShoppingCart } from "lucide-react";
+import { Minus, Plus, ShoppingCart } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { addCart } from "@/store/cartSlice";
 import { toast } from "react-toastify";
+import { Input } from "../ui/input";
 
 const ProductCard = ({ data }: { data: Product }) => {
   const dispatch = useDispatch();
@@ -24,10 +25,21 @@ const ProductCard = ({ data }: { data: Product }) => {
           <CardTitle className="text-lg">{data.title}</CardTitle>
           <CardDescription className="font-medium text-lg pl-4">${data.price}</CardDescription>
         </div>
-        <Button onClick={() => handleAddToCart(data)}>
-          <ShoppingCart className="mr-2 h-5" />
-          Add Cart
-        </Button>
+        <div className="flex items-center gap-2 flex-col md:flex-row">
+          <div className="flex items-center gap-4">
+            <Button>
+              <Minus />
+            </Button>
+            <Input className="w-full text-center" />
+            <Button>
+              <Plus />
+            </Button>
+          </div>
+          <Button onClick={() => handleAddToCart(data)} className="w-full md:w-auto">
+            <ShoppingCart className="mr-2 h-5" />
+            Add Cart
+          </Button>
+        </div>
       </CardContent>
     </Card>
   );
